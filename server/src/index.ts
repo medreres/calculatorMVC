10e6efeed85587f52299ad40eebc170dea487ed5
Maksym Prygoda <maxum23025@gmail.com>
2023-03-29 11:54:56 +0300

    Fix .env undefined variable.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,14 @@
 import cors from "cors";
 import express from "express";
 import { calculatorRoutes } from "./routes/calculator";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-// TODO small doc for api
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "*" }));
 app.use(calculatorRoutes);
 
-// ? both port could be already used?
 const port = process.env.SERVER_PORT || 7890;
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
